8669d606fa125e1c6dee29a38982cb618d44853c
Trần Văn Lương <luonguet96@gmail.com>
2016-05-06 05:30:34 +0000

    Initialize repository

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,62 @@
 # Part 1
 
 def sum arr
+  sum = 0
+  if arr.empty?
+    return 0
+  end
+  if arr.length ==1 
+    return arr[0]
+  end
+  arr.each do |x|
+    sum+=x
+  end
+  return sum
   # YOUR CODE HERE
 end
 
 def max_2_sum arr
+ sum arr.sort.pop(2)
   # YOUR CODE HERE
 end
 
 def sum_to_n? arr, n
+  arr.combination(2).each {|x| return true if (x[0]+x[1] == n)}
+  false
   # YOUR CODE HERE
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+   "Hello, #{name}"
+
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s=~ /^[a-z&&[^aeiou]]/i
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/ 
+    s.to_i(2) %4 == 0
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  attr_accessor :isbn
+  attr_accessor :price
+  def price_as_string
+      "$#{sprintf("%.2f",@price)}"
+  end
+
 end
